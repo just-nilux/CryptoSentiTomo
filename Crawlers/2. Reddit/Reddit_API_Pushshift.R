4fb2451e6bd2fbefbfa8d34cd44591c64e14a1ff
Nguyen Ngoc Nam Trung <trungnnn1908@gmail.com>
2017-11-01 19:57:42 +0100

    Finalize Reddit Crawler
    - Using Pushshift.io API to crawl data (high rate limit)
    - Script to crawl data base on epoch time (can go back further to the
    past if want)
    - Data for Reddit Oct 2017 collected. Only submissions (no comments)

---
+++ Crawlers/2. Reddit/Reddit_API_Pushshift.R
@@ -0,0 +1,126 @@
+# Try APIs call on https://elasticsearch.pushshift.io
+
+#Set up working directory
+#setwd("C:/Users/BluePhoenix/Documents/GitHub/NextBigCrypto-Senti/Crawlers")
+setwd("~/GitHub/NextBigCrypto-Senti/Crawlers")
+
+# Clear environment
+rm(list = ls())
+
+# install packages if not available
+packages <- c("jsonlite","httr", # support API call
+              "dplyr")
+
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
+lapply(packages, require, character.only = TRUE)
+
+#--------------------------------#
+# GENESIS CODE (run only 1 time) #
+#--------------------------------#
+# 
+# url <- "https://api.pushshift.io"
+# path <- "/reddit/search/submission/"
+# query <- "?q=&subreddit=cryptocurrency&after=1506816000&stickied=false&locked=false&is_video=false&size=500"
+# 
+# # Extract Reddit submissions using filters
+# # - Subreddit = cryptocurrency
+# # - After = 1st Oct 2017 (epoch time)
+# # - No sticked, locked and video submissions
+# # - Size = 500 (maximum per call)
+# 
+# raw_data <- GET(url = url,
+#                 path = path,
+#                 query = query)
+# names(raw_data)
+# raw_data$status_code #if status = 200 -> fine
+# 
+# test <- rawToChar(raw_data$content)
+# 
+# # extract JSON type from raw_data
+# extract.data <- fromJSON(test)
+# class(extract.data)
+# 
+# # convert to dataframe
+# reddit.df <- do.call(what = "rbind",
+#                      args = lapply(extract.data, as.data.frame))
+# # drop nested columns
+# reddit.df <- reddit.df[ , -which(names(reddit.df) %in%
+#                                    c("media","media_embed","preview",
+#                                      "secure_media","secure_media_embed"))]
+# 
+# # save genesis dataset
+# write.csv(reddit.df,paste0(report_path,'Crypto_Reddit.csv'))
+
+#---------------------------------------------------------------------
+
+# Convert epoch time
+unix2POSIXct <- function(time) structure(time, class = c("POSIXt", "POSIXct"))
+
+# Develop function to re-loop crawl from pushshift.io
+# base on last epoch date available in the dataset up to current date
+
+crawl_reddit_pushshift <- function(epoch){
+  url <- "https://api.pushshift.io"
+  path <- "/reddit/search/submission/"
+  query <- "?q=&subreddit=cryptocurrency&stickied=false&locked=false&is_video=false&size=500"
+  
+  # query results after epoch time
+  query <- paste0(query,'&after=',epoch)
+  
+  # extract data from API
+  raw_data <- GET(url = url,
+                  path = path,
+                  query = query)
+  
+  # will implement confirm mechanism later if status code <> 200
+  raw_data$status_code #if status = 200 -> fine
+  
+  char_data <- rawToChar(raw_data$content)
+  
+  # extract JSON type from raw_data
+  extract.data <- fromJSON(char_data)
+  
+  # convert to dataframe
+  final.df <- do.call(what = "rbind",
+                      args = lapply(extract.data, as.data.frame))
+  # drop nested columns 
+  final.df <- final.df[ , -which(names(final.df) %in% 
+                                   c("media","media_embed","preview",
+                                     "secure_media","secure_media_embed"))]
+  return(final.df)
+}
+
+end_epoch <- as.numeric('1509494400') #1st Nov 00:00
+
+# Continue to crawl Reddit until epoch time reach 1st Nov
+repeat{
+  
+  # Extract data from previous report
+  report_path <- '2b. Reddit Report/'
+  old.df <- read.csv(paste0(report_path,"Crypto_Reddit.csv"))
+  old.df <- old.df[ , -which(names(old.df) %in% c("X"))]
+  
+  # extract latest epoch time from old dataset
+  max_epoch <- as.numeric(max(old.df$created_utc))
+  
+  # Start crawling and save to "Crypto_Reddit.csv"
+  new.df <- crawl_reddit_pushshift(max_epoch)
+  
+  final.df <- bind_rows(old.df,new.df)
+  
+  # Save new.df base on epoch
+  write.csv(new.df,paste0(report_path,'Crypto_Reddit_',
+                          substr(unix2POSIXct(max_epoch),0,10)
+                          ,'.csv'))
+ 
+  # Overwrite final file
+  write.csv(final.df,paste0(report_path,'Crypto_Reddit.csv'))
+  if(max_epoch >= end_epoch){
+    break
+  }
+}
+
+# ----------------------------------
+
--- Crawlers/x. Outdate/Reddit_v1.R
+++ Crawlers/x. Outdate/Reddit_v1.R
@@ -15,11 +15,25 @@ lapply(packages, require, character.only = TRUE)
 
 #############################################
 
-
-CryptoCurrency_subreddit <- get_reddit(search_terms = NA,
+crypto_3010     <- get_reddit(
+                   search_terms = NA,
                    subreddit = 'CryptoCurrency',
-                   page_threshold = 10)
-##############################
+                   page_threshold = 1000,
+                   cn_threshold = 0,
+                   wait_time = 2 )
+# ##############################
+# old <- read.csv("./Sub_Crypto_Oct29.csv")
+# old <- old[,-1]
+# final <- rbind(old,crypto_3010)
+# final <- unique(final)
+# write.csv(crypto_3010, "Sub_Crypto_Oct30.csv")
+# write.csv(final,"Reddit_crypto_full.csv")
+
+#######################
+
+Crypto.Sub <- read.csv("Reddit_crypto_full.csv",
+                       as.is = TRUE)
+Crypto.Sub <- Crypto.Sub[,-1]
 
 POST("https://ssl.reddit.com/api/v1/access_token",
      body = list(
